18155e112fa4da6af66c05e6a65475d4c2313d4f
jd-galvan <jdgsuazo@gmail.com>
2026-04-06 15:14:42 +0200

    Fix sorting

--- src/app/api/transactions/route.js
+++ src/app/api/transactions/route.js
@@ -19,7 +19,7 @@ export async function GET(request) {
     })
     .from(transactions)
     .leftJoin(categories, eq(transactions.categoryId, categories.id))
-    .orderBy(desc(transactions.createdAt));
+    .orderBy(desc(transactions.date), desc(transactions.createdAt));
 
   if (month) {
     const [year, m] = month.split("-").map(Number);
